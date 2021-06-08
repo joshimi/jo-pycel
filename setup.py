db24410ff51634c6b44f9fc66359106353c7e896
Yonatan Banian <yonatan@joshuins.com>
2021-06-08 17:47:34 +0300

    removed the toady() func in excellib.py and upgreded version tag in setup.py

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ with open('test-requirements.txt') as f:
 
 setup(
     name='pycel',
-    version='1.0.6',
+    version='1.0.7',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     description='A library for compiling excel spreadsheets to python code '
@@ -64,7 +64,7 @@ setup(
         'networkx>=2.0,<2.7',
         'numpy>=1.20.1',
         'scipy>=1.6.1',
-        'openpyxl==3.0.5',
+        'openpyxl==3.0.7',
         'python-dateutil',
         'ruamel.yaml',
     ],
